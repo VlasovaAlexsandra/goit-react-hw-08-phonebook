cbaa18d528cbbca7568ba53cf5f73a39d3f798a9
VlasovaAlexsandra <68018046+VlasovaAlexsandra@users.noreply.github.com>
2021-05-06 01:00:17 +0300

    add router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React, { Component } from 'react';
-import ContactForm from './components/Form/ContactForm/ContactForm';
-import Filter from './components/Form/Filter/Filter';
-import ContactList from './components/Form/ContactList/ContactList';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom'
+import AppBar from './components/AppBar'
+import ContactsView from './Views/ContactsView'
+import HomeView from './Views/HomeView'
+import RegisterView from './Views/RegisterView'
+import LoginView from './Views/LoginView'
+import Container from './components/Container/index'
+
+// import ContactForm from './components/Form/ContactForm/ContactForm';
+// import Filter from './components/Form/Filter/Filter';
+// import ContactList from './components/Form/ContactList/ContactList';
 import './App.css'
 
-class App extends Component {
 
-  render() {
+const App = () => (
+  <Container>
+    <AppBar />
+    <Switch>
+      <Route exact path="/" component={HomeView} />
+      <Route path="/register" component={RegisterView} />
+      <Route path="/login" component={LoginView} />
+      <Route path="/contacts" component={ContactsView} />
 
-    return (
-      <>
-        <h1 className="Title">Phonebook</h1>
-        <ContactForm />
-        <h2 className="Title">Contacts</h2>
-        <Filter />
-        <ContactList />
-      </>
 
-    )
-  }
-}
+    </Switch>
+
+  </Container>
+)
+// class App extends Component {
+
+//   render() {
+
+//     return (
+//       <>
+//         <h1 className="Title">Phonebook</h1>
+//         <ContactForm />
+//         <h2 className="Title">Contacts</h2>
+//         <Filter />
+//         <ContactList />
+//       </>
+
+//     )
+//   }
+// }
 
 export default App;
